af7ed478b72ee2736642332f863c1deab59210ec
Noor E Hira <hira6386@gmail.com>
2026-01-27 09:10:13 +0500

    commit

--- lab02/Task1.s
+++ lab02/Task1.s
@@ -1,20 +1,23 @@
 .text
 .globl main
 main:
+    #Listing 3
 
-    # addi x19,x0,5 #i
-    # addi x20,x0,5 #j
-    # addi x21,x0,6 #g
-    # addi x22,x0,8 #h
-    # addi x23,x0,0 #f
+    addi x19,x0,5 #i
+    addi x20,x0,5 #j
+    addi x21,x0,6 #g
+    addi x22,x0,8 #h
+    addi x23,x0,0 #f
 
-    # bne x19,x20,Else
-    # add x23,x21,x22
-    # beq x0,x0,Exit
+    bne x19,x20,Else
+    add x23,x21,x22
+    beq x0,x0,Exit
 
-    # Else: sub x23,x21,x22
+    Else: sub x23,x21,x22
 
-    # Exit: 
+    Exit: 
+
+    #Listing 4
 
     // assuming i in x22 and k in x24, and the base address of save in x25
     Loop:
@@ -25,22 +28,6 @@ main:
         addi x22, x22, 1
         beq  x0, x0, Loop
     Exit:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
